9b8a83b8bb4fba45bca03b5dc23be0c017c3963b
ricky23i <rduenas2@illinois.edu>
2021-12-23 17:02:51 -0600

    requestserviceimpl

---
+++ trms-back/src/main/java/com/revature/services/RequestReviewServiceImpl.java
@@ -0,0 +1,41 @@
+package com.revature.services;
+
+import java.util.Set;
+
+import com.revature.beans.Comment;
+import com.revature.beans.Employee;
+import com.revature.beans.Reimbursement;
+import com.revature.beans.Status;
+import com.revature.data.ReimbursementDAO;
+import com.revature.data.postgres.ReimbursementPostgres;
+
+public class RequestReviewServiceImpl implements RequestReviewService{
+	private ReimbursementDAO reimbDao= new ReimbursementPostgres();
+	@Override
+	public Set<Reimbursement> getPendingReimbursements(Employee approver) {
+		// TODO Auto-generated method stub
+		
+		return (reimbDao.getByRequestor(approver));
+	}
+
+	@Override
+	public void approveRequest(Reimbursement request) {
+		Status approvestat =request.getStatus();
+		approvestat.setName("Approved");
+		request.setStatus(approvestat);
+		reimbDao.update(request);
+	}
+
+	@Override
+	public void rejectRequest(Reimbursement request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rejectRequest(Reimbursement request, Comment comment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
