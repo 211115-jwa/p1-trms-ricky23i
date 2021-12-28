a8e4b6d6e29d94027dd866b95dd8738b4d5b9dea
ricky23i <rduenas2@illinois.edu>
2021-12-28 13:44:50 -0600

    front added

--- trms-back/src/main/resources/TRMS_dml.sql
+++ trms-back/src/main/resources/TRMS_dml.sql
@@ -13,8 +13,8 @@ values(default,'university course','80'),
 (default,'technical training','90'),
 (default,'other','30');
 select * from event_type;
-*/
-/*insert into status(status_id,status_name,approver)
+
+insert into status(status_id,status_name,approver)
 values(default, 'pending approval','direct supervisor'),
 (default, 'approved','direct supervisor'),
 (default, 'denied','direct supervisor'),
@@ -24,7 +24,5 @@ values(default, 'pending approval','direct supervisor'),
 (default, 'pending approval','benefits coordinator'),
 (default, 'approved','benefits coordinator'),
 (default, 'denied','benefits coordinator');
-
 select * from status;
 */
-
