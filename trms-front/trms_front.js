f1bb7eadc6f74b653f219c4cb16d6fa4c743ab17
ricky23i <rduenas2@illinois.edu>
2022-01-02 22:53:55 -0600

    add pending requests to front end

--- trms-front/trms_front.js
+++ trms-front/trms_front.js
@@ -1,4 +1,17 @@
-﻿
+﻿let trmsAppUrl = 'http://localhost:8080/';
+setupNav();
+function setupNav() {
+    let nav = document.getElementById('nav');
+      nav.innerHTML = `<span id="navLeft">
+        <a href="trms_front.html"><b>Tuition Reimbursement Form</b></a>
+        <a href="request.html"><b>Pending Requests</b></a>
+        </span>
+        <span id="navRight">
+        <a href="#">Log In</a>
+        </span>`;
+    }
+
+
 function submitReq() {
     console.log('Submit')
 }
